56b10a3cb14c3549a73db3ec27dd47b23264d956
Jimmy Reichley <jimmyqpublik@gmail.com>
2022-03-16 22:54:08 -0400

    NavPage as root

--- Apps/Noctal/Base/NavigationCoordinators/ApplicationCoordinator.cs
+++ Apps/Noctal/Base/NavigationCoordinators/ApplicationCoordinator.cs
@@ -4,9 +4,21 @@ public class ApplicationCoordinator : BaseCoordinator
 {
     public Page RootPage { get; private set; }
 
+    private NavigationPage NavPage { get; } = new NavigationPage();
+
     public ApplicationCoordinator()
     {
+        RootPage = NavPage;
+    }
+
+    public override async Task Start()
+    {
+        await base.Start();
+
         var page = new FeedView();
-        RootPage = page;
+        page.ViewModel = new();
+        page.Initialize();
+
+        await NavPage.PushAsync(page, false);
     }
 }
